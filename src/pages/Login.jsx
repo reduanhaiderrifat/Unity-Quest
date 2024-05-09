5656f233c6f88c3e035744a04e5e8e97a3839ed8
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-05-10 00:32:03 +0600

    create login  & register & authProvider & setup functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,148 +1,148 @@
-// import { useForm } from "react-hook-form";
-// import { FaGithub } from "react-icons/fa";
-// import { FcGoogle } from "react-icons/fc";
-// import { Link } from "react-router-dom";
-// import { useState } from "react";
-// import { IoMdArrowRoundBack } from "react-icons/io";
-// import useAuth from "../hooks/useAuth";
-// import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import useAuth from "../hooks/useAuth";
+import toast from "react-hot-toast";
 
-// const Login = () => {
-//   const { singInUser, googleUser, githubUser } = useAuth();
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm();
+const Login = () => {
+  const { singInUser, googleUser, githubUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-//   const [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
 
-//   const onSubmit = (data) => {
-//     const { email, password } = data;
-//     console.log(data);
-//     singInUser(email, password)
-//       .then((result) => {
-//         console.log(result.user);
-//         toast.success("User login successfully");
-//       })
-//       .catch((error) => {
-//         toast.error(error.message);
-//       });
-//   };
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    console.log(data);
+    singInUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        toast.success("User login successfully");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
-//   return (
-//     <div>
-//       <div className="bg-[#00684A]">
-//         <div className="hero-content flex-col md:flex-row-reverse lg:flex-row-reverse">
-//           <div className="w-2/3 hidden md:flex lg:flex xl:flex">
-//             <img
-//               className="w-full h-full"
-//               src="https://i.ibb.co/X4sY8KS/image.png"
-//               alt=""
-//             />
-//           </div>
-//           <div className="card shrink-0 w-full  max-w-sm  bg-base-100">
-//             <div className="card-body">
-//               <h1 className="text-3xl font-bold text-[#00684A]">
-//                 Log in to your account
-//               </h1>
-//               <p className="font-semibold">
-//                 Don't have an account?{" "}
-//                 <Link to="/register" className=" text-[#016BF9]">
-//                   Sign Up
-//                 </Link>
-//               </p>
-//             </div>
-//             <div className="flex  flex-col gap-3 mx-8">
-//               <button
-//                 onClick={googleUser}
-//                 className="btn w-full bg-transparent hover:bg-transparent"
-//               >
-//                 <FcGoogle size={30} /> Google
-//               </button>
-//               <button
-//                 onClick={githubUser}
-//                 className="btn w-full bg-transparent hover:bg-transparent"
-//               >
-//                 <FaGithub size={30} /> GitHub
-//               </button>
-//             </div>
-//             <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-//               {step === 1 && (
-//                 <div className="form-control">
-//                   <label className="label">
-//                     <span className="label-text">Email</span>
-//                   </label>
-//                   <input
-//                     type="email"
-//                     placeholder="email"
-//                     name="email"
-//                     className="input input-bordered"
-//                     {...register("email", {
-//                       required: "This field is required",
-//                     })}
-//                   />
-//                   {errors.email && (
-//                     <span className="text-red-500">{errors.email.message}</span>
-//                   )}
-//                 </div>
-//               )}
-//               {step === 2 && (
-//                 <div className="form-control">
-//                   <label className="label">
-//                     <span className="label-text">Password</span>
-//                   </label>
-//                   <input
-//                     type="password"
-//                     placeholder="password"
-//                     name="password"
-//                     className="input input-bordered"
-//                     {...register("password", {
-//                       required: "This field is required",
-//                     })}
-//                   />
-//                   {errors.password && (
-//                     <span className="text-red-500">
-//                       {errors.password.message}
-//                     </span>
-//                   )}
-//                   <label className="label">
-//                     <p className="label-text-alt link link-hover">
-//                       Forgot password?
-//                     </p>
-//                   </label>
-//                 </div>
-//               )}
-//               <div className="form-control mt-6 relative">
-//                 {step === 1 ? (
-//                   <button
-//                     type="button"
-//                     onClick={() => setStep(2)}
-//                     className="btn btn-primary"
-//                   >
-//                     Next
-//                   </button>
-//                 ) : (
-//                   <>
-//                     <button
-//                       type="button"
-//                       onClick={() => setStep(1)}
-//                       className=" rounded-full border p-2 absolute right-0 bottom-16"
-//                     >
-//                       <IoMdArrowRoundBack size={25} />
-//                     </button>
-//                     <button type="submit" className="btn btn-primary">
-//                       Login
-//                     </button>
-//                   </>
-//                 )}
-//               </div>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <div className="bg-[#00684A]">
+        <div className="hero-content flex-col md:flex-row-reverse lg:flex-row-reverse">
+          <div className="w-2/3 hidden md:flex lg:flex xl:flex">
+            <img
+              className="w-full h-full"
+              src="https://i.ibb.co/X4sY8KS/image.png"
+              alt=""
+            />
+          </div>
+          <div className="card shrink-0 w-full  max-w-sm  bg-base-100">
+            <div className="card-body">
+              <h1 className="text-3xl font-bold text-[#00684A]">
+                Log in to your account
+              </h1>
+              <p className="font-semibold">
+                Don't have an account?{" "}
+                <Link to="/register" className=" text-[#016BF9]">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+            <div className="flex  flex-col gap-3 mx-8">
+              <button
+                onClick={googleUser}
+                className="btn w-full bg-transparent hover:bg-transparent"
+              >
+                <FcGoogle size={30} /> Google
+              </button>
+              <button
+                onClick={githubUser}
+                className="btn w-full bg-transparent hover:bg-transparent"
+              >
+                <FaGithub size={30} /> GitHub
+              </button>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              {step === 1 && (
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered"
+                    {...register("email", {
+                      required: "This field is required",
+                    })}
+                  />
+                  {errors.email && (
+                    <span className="text-red-500">{errors.email.message}</span>
+                  )}
+                </div>
+              )}
+              {step === 2 && (
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    className="input input-bordered"
+                    {...register("password", {
+                      required: "This field is required",
+                    })}
+                  />
+                  {errors.password && (
+                    <span className="text-red-500">
+                      {errors.password.message}
+                    </span>
+                  )}
+                  <label className="label">
+                    <p className="label-text-alt link link-hover">
+                      Forgot password?
+                    </p>
+                  </label>
+                </div>
+              )}
+              <div className="form-control mt-6 relative">
+                {step === 1 ? (
+                  <button
+                    type="button"
+                    onClick={() => setStep(2)}
+                    className="btn btn-primary"
+                  >
+                    Next
+                  </button>
+                ) : (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => setStep(1)}
+                      className=" rounded-full border p-2 absolute right-0 bottom-16"
+                    >
+                      <IoMdArrowRoundBack size={25} />
+                    </button>
+                    <button type="submit" className="btn btn-primary">
+                      Login
+                    </button>
+                  </>
+                )}
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Login;
+export default Login;
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,72 +1,72 @@
-// import {
-//   GithubAuthProvider,
-//   GoogleAuthProvider,
-//   createUserWithEmailAndPassword,
-//   onAuthStateChanged,
-//   signInWithEmailAndPassword,
-//   signInWithPopup,
-//   signOut,
-// } from "firebase/auth";
-// import { createContext, useEffect, useState } from "react";
-// import auth from "../firebase/firebase.config";
-// import toast from "react-hot-toast";
-// export const AuthContext = createContext(null);
-// const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const googleProvider = new GoogleAuthProvider();
-//   const githubProvider = new GithubAuthProvider();
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+import toast from "react-hot-toast";
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-//   const googleUser = () => {
-//     return signInWithPopup(auth, googleProvider)
-//       .then((result) => {
-//         console.log(result.user);
-//         toast.success("User login with google successfully");
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         toast.error(error.message);
-//       });
-//   };
-//   const githubUser = () => {
-//     return signInWithPopup(auth, githubProvider)
-//       .then((result) => {
-//         console.log(result.user);
-//         toast.success("User login with github successfully");
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         toast.error(error.message);
-//       });
-//   };
-//   const createUser = (email, password) => {
-//     return createUserWithEmailAndPassword(auth, email, password);
-//   };
-//   const singInUser = (email, password) => {
-//     return signInWithEmailAndPassword(auth, email, password);
-//   };
-//   const logOut = () => {
-//     return signOut(auth);
-//   };
-//   useEffect(() => {
-//     const unsuscribe = onAuthStateChanged(auth, (currentuser) => {
-//       setUser(currentuser);
-//       setLoading(false);
-//     });
-//     return () => unsuscribe();
-//   }, []);
-//   const authInfo = {
-//     createUser,
-//     singInUser,
-//     googleUser,
-//     githubUser,
-//     logOut,
-//     loading,
-//     user,
-//   };
-//   return (
-//     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-//   );
-// };
+  const googleUser = () => {
+    return signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        toast.success("User login with google successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
+  const githubUser = () => {
+    return signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        console.log(result.user);
+        toast.success("User login with github successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const singInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsuscribe = onAuthStateChanged(auth, (currentuser) => {
+      setUser(currentuser);
+      setLoading(false);
+    });
+    return () => unsuscribe();
+  }, []);
+  const authInfo = {
+    createUser,
+    singInUser,
+    googleUser,
+    githubUser,
+    logOut,
+    loading,
+    user,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
 
-// export default AuthProvider;
+export default AuthProvider;
