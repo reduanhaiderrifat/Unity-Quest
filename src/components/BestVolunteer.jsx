3423e973af685761400e6763d8734e4c991f2409
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-05-15 17:56:04 +0600

    add one more extra section

---
+++ src/components/BestVolunteer.jsx
@@ -0,0 +1,363 @@
+import { FaFacebook, FaStar, FaTwitter } from "react-icons/fa6";
+import { GrLinkedin } from "react-icons/gr";
+import { MdMarkEmailUnread } from "react-icons/md";
+
+const BestVolunteer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-20">
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/RyVSdVX/profile1.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">David Patel</h2>
+              <p className="px-5 text-xs sm:text-base ">
+                David is a dedicated advocate for social justice and equality.
+                He volunteers at local shelters, soup kitchens, and community
+                centers, providing support and resources to marginalized
+                individuals and families.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/syR073X/profile2.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">Aisha Khan</h2>
+              <p className="px-5 text-xs sm:text-base ">
+                Aisha is a committed mentor and educator who volunteers her time
+                tutoring children from underserved communities. She believes in
+                the power of education to break the cycle of poverty and empower
+                youth to achieve their dreams.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/v1RKgL1/profil6.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">
+                Carlos Rodriguez
+              </h2>
+              <p className="px-5 text-xs sm:text-base ">
+                Carlos is an enthusiastic animal lover who volunteers at the
+                local animal shelter, helping to care for and socialize rescue
+                animals. He believes that every animal deserves love and a
+                forever home.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/rMqV1ty/profile3.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">Lily Nguyen</h2>
+              <p className="px-5 text-xs sm:text-base ">
+                Lily is a compassionate healthcare volunteer who dedicates her
+                time to supporting patients and their families at the local
+                hospital. She provides comfort, companionship, and assistance
+                with daily tasks to improve the hospital experience for all.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/2qx6RKq/profile4.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">Ravi Gupta</h2>
+              <p className="px-5 text-xs sm:text-base ">
+                Ravi is a skilled IT professional who volunteers his expertise
+                to help nonprofit organizations with website development, tech
+                support, and digital marketing strategies. He believes in
+                leveraging technology for positive social impact and community
+                development.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hover:shadow-2xl">
+        <div className="flex flex-col justify-center  p-6 shadow-md rounded-xl sm:px-12 ">
+          <img
+            src="https://i.ibb.co/YPQ1tpb/profile5.jpg"
+            alt=""
+            className="w-44 h-44 mx-auto rounded-full  aspect-square"
+          />
+          <div className="space-y-4 text-center divide-y ">
+            <div className="flex items-center justify-center gap-1 mt-3 text-orange-500">
+              <FaStar /> <FaStar /> <FaStar /> <FaStar />
+              <FaStar />
+            </div>
+            <div className="my-2 space-y-1">
+              <h2 className="text-xl font-semibold sm:text-2xl">Emily Chang</h2>
+              <p className="px-5 text-xs sm:text-base ">
+                Emily is a passionate environmentalist who has been involved in
+                various tree planting and beach cleanup initiatives in her
+                community. She believes in the importance of preserving nature
+                for future generations.
+              </p>
+            </div>
+            <div className="flex justify-center pt-2 space-x-4 align-center">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Linkin"
+                className="p-2 rounded-md "
+              >
+                <GrLinkedin size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Twitter"
+                className="p-2 rounded-md "
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Email"
+                className="p-2 rounded-md "
+              >
+                <MdMarkEmailUnread size={20} />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Facebook"
+                className="p-2 rounded-md "
+              >
+                <FaFacebook size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BestVolunteer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Slider from "../components/Slider";
 import VolunteerNeedCard from "../components/VolunteerNeedCard";
 import { Link } from "react-router-dom";
 import Charity from "../components/Charity";
+import BestVolunteer from "../components/BestVolunteer";
 
 const Home = () => {
   return (
@@ -34,26 +35,35 @@ const Home = () => {
           <div className=" w-full flex justify-center mt-12">
             <Link
               to="/need"
-              className="btn w-1/3 shadow-black box-shadow  text-xl lg:text-3xl font-bold bg-300% bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
+              className="btn w-2/3 md:w-1/3 shadow-black box-shadow  text-xl lg:text-3xl font-bold bg-300% bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
             >
               See All Post
             </Link>
           </div>
         </section>
-        <section className="mt-14 lg:mt-44">
-          <LearnMore />
+        <section className="mt-20 font-semibold">
+          <div className="text-center ">
+            <h2 className="text-3xl md:text-5xl">
+              <span className="text-green-500">Our best</span> volunteer of
+              these year
+            </h2>
+            <BestVolunteer />
+          </div>
         </section>
         <section className="mt-14 lg:mt-44">
-          <h2 className="text-3xl mb-16 font-bold text-center">
-            Make A Difference Today By <br /> Donating To Our Cause.
-          </h2>
-          <Donate />
+          <LearnMore />
         </section>
         <section className="my-24 ">
           <h2 className="text-3xl mb-16 font-bold text-center">
             Discover Our Updates & Charity <br /> News Content.
           </h2>
           <Charity />
+        </section>
+        <section className="mt-14 lg:mt-44">
+          <h2 className="text-3xl mb-16 font-bold text-center">
+            Make A Difference Today By <br /> Donating To Our Cause.
+          </h2>
+          <Donate />
         </section>
       </div>
     </>
